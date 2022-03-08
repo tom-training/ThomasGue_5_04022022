aabb3d86590e385d7ba00d316e2206c744aba76e
tom-training <thomasgue77@gmail.com>
2022-03-08 22:40:46 +0100

    debuggage de la commande d'un canapé déjà présent même couleur

--- js/product.js
+++ js/product.js
@@ -2,11 +2,9 @@ const leDivImage = document.querySelector(".item__img");
 
 const selectionCouleur = document.getElementById("colors");
 
-// la variable str nous permet de récupérer l'URL
+// la variable str (string) nous permet de récupérer l'URL
 
 var str = window.location.href;
-
-//console.log(str);
 
 // Avec un constructeur on crée une instance de classe qu'on nomme url
 var url = new URL(str);
@@ -16,9 +14,8 @@ dans l'URL */
 
 var identifiant = url.searchParams.get("id");
 
-//console.log(identifiant);
 
-/* On commence par un fetch avec une promise
+/* On commence par un fetch qui sera une promise,
 
 quand on a récupéré les valeurs/variables de l'API
 
@@ -41,11 +38,8 @@ fetch("http://localhost:3000/api/products")
         const tableauData = value;
 
         for(const element of tableauData){
-            //console.log(element);
 
             if(element._id === identifiant){
-                /* console.log("C'est le canapé avec l'identifiant "+ element._id + 
-                " qui est le canapé: "+ element.name);  */
 
                 // c'est ici que tout se joue au niveau insertion des éléments du canape
 
@@ -70,69 +64,46 @@ fetch("http://localhost:3000/api/products")
                     selectionCouleur.appendChild(couleurOption);
                 }
 
-            /*  }else{
+                /*  }else{
                 console.log("c'est pas lui !!!!");
-                }                                           */
+                }   */
             }    
         }
     })
     .catch(function(err){
-        console.log(err);
+
+        console.error(err);
+        const messageErreur = document.createElement("p");
+    
+        messageErreur.textContent = "Il semblerait qu'un problème technique empêche le " + 
+        "chargement des informations concernant nos canapés, " + "nous nous excusons pour ce désagrément," 
+        +" veuillez réactualiser la page";
+
+        document.querySelector(".item").appendChild(messageErreur);
+
     });
 
 // FIN DE LA REQUETE FETCH ------- FIN DE LA REQUETE FETCH  ------- FIN DE LA REQUETE FETCH  
 
-/* Avant qu'on mette dans le panier, on scanne le panier pour verifier s'il n'y a pas déja 
-    un produit de meme couleur/meme produit (id et couleur identique) ==> addition sinon 
-    ajout du nouveau produit
+/* 
 
-tester les cas d'erreur si 0 mis en quantité et si pas de couleurs   < C'EST FAIT >
+Avant qu'on mette dans le panier, on scanne le panier pour verifier s'il n'y a pas déja 
+un produit de meme couleur/meme produit (id et couleur identique) ==> addition sinon 
+ajout du nouveau produit
 
-/* C'est ici que l'on récupère la quantité et la couleur sélectionnée
+tester les cas d'erreur si 0 mis en quantité et si pas de couleurs  
+
+C'est ici que l'on récupère la quantité et la couleur sélectionnée
 Cette récupération se fera au moment où le visiteur clique sur "commander"
 avec un eventListener sur le button id addToCart
-
 */
-console.log(localStorage);
+
+// on va vérifier s'il y a qqchose dans le localStorage
 
 const monStockageJSON = localStorage.getItem("obj");
 const monStockageJS = JSON.parse(monStockageJSON);
 
-
-if(monStockageJS){
-    if(monStockageJS.length === 0){
-        console.log("le panier est vide");
-
-    }else{
-        console.log('il y a qqchose dans le panier');
-        console.log(monStockageJSON);
-        console.log(monStockageJS);  
-        
-        for(let i=0; i<monStockageJS.length; i++){
-            console.log(monStockageJS[i].idt);
-        }
-        
-    }
-}else{
-    console.log('le localStorage est null');
-
-}
-                
-    /*
-    for (let i=0; i<monStockageJS.length; i++){
-    
-        console.log(monStockageJS[i]);
-    
-        console.log("Le produit commandé est le "+ monStockageJS[i].idt + "en "+ 
-        monStockageJS[i].nombre + " exemplaires, de couleur: " + monStockageJS[i].couleur);
-    
-    }
-*/
-
-// ATTENTION SURVEILLER CET OBJET DÉCLARÉ, peut-être faut il l'inclure dans l'event
-
-// Il faut un tableau d'objets
-
+console.log(monStockageJS);
 
 let monPanierTab = [
 
@@ -150,26 +121,17 @@ let monPanierTab = [
 document.getElementById("addToCart").addEventListener("click", function(){
 
     // ici on récupère la valeur couleur et la valeur nombre
-
-    console.log(identifiant);
-    console.log(document.getElementById("colors").value); 
-    console.log(document.getElementById("quantity").value);
-
     // il faut les stocker
   
-
-    let leNombreSelect = document.getElementById("quantity").value;
+    let leNombreSelect = Number(document.getElementById("quantity").value);
+    console.log(typeof(leNombreSelect));
     let laCouleurSelect = document.getElementById("colors").value;
     let leProduitSelect = identifiant;
 
-
-    // On va aller chercher l'objet LOCALSTORAGE
-
-    // on va aller chercher ce qu'il y a dans le localStorage
-
+    // On va aller chercher l'objet dans le LOCALSTORAGE
     // On va boucler cet Objet (s'il existe)
 
-    if(leNombreSelect>0 && laCouleurSelect){    //on verifie qu'il y a au moins un canap et une couleur selectionnée
+    if(leNombreSelect>0 && laCouleurSelect){ //on verifie qu'il y a au moins un canap et une couleur selectionnée
 
         if(monStockageJS){
             if(monStockageJS.length === 0){
@@ -193,25 +155,28 @@ document.getElementById("addToCart").addEventListener("click", function(){
         
             }else{
                 console.log('il y a qqchose dans le panier');
-                console.log(monStockageJSON);
-                console.log(monStockageJS); 
 
                 let additionArticle = false;
 
-                // s'il y a déjà un canapé même couleur et même identifiant on ne fait qu'ajouter les nombres nouveau et anciens
-
+                // s'il y a déjà un canapé même couleur et même identifiant on ne fait qu'ajouter 
+                // les nombres nouveau et anciens
                 // sinon on déroule le code où l'on ajoute un objet
+
                 for(let i=0; i<monStockageJS.length; i++){
 
                     let idtAlready = monStockageJS[i].idt;
                     let couleurAlready = monStockageJS[i].couleur;
 
                     if(idtAlready === leProduitSelect && couleurAlready === laCouleurSelect){
-                        // code où l'on ajoute
-
-                        monStockageJS[i] = monStockageJS[i] + leNombreSelect;
+                        
+                        // code où l'on ajoute en ne faisant qu'une simple addition
+                        monStockageJS[i].nombre = Number(monStockageJS[i].nombre) + leNombreSelect;
                         additionArticle = true;
 
+                        let monPanierJSon = JSON.stringify(monStockageJS);
+                        localStorage.setItem("obj", monPanierJSon);
+
+                        
                     }else{
                         console.log("non il n'y a pas eu de commande de canapé de ce type avant");
                     }
@@ -220,9 +185,9 @@ document.getElementById("addToCart").addEventListener("click", function(){
                 if(additionArticle === false){
                     monStockageJS.push( 
                         {
-                        idt : leProduitSelect,
-                        nombre: leNombreSelect,
-                        couleur: laCouleurSelect
+                            idt : leProduitSelect,
+                            nombre: leNombreSelect,
+                            couleur: laCouleurSelect
                         }
                     );
 
@@ -236,9 +201,9 @@ document.getElementById("addToCart").addEventListener("click", function(){
 
             monPanierTab= [
                 {
-                idt : leProduitSelect,
-                nombre: leNombreSelect,
-                couleur: laCouleurSelect
+                    idt : leProduitSelect,
+                    nombre: leNombreSelect,
+                    couleur: laCouleurSelect
                 }
             ];    
 
@@ -255,13 +220,6 @@ document.getElementById("addToCart").addEventListener("click", function(){
     }else{
         console.log("choisissez la couleur et le nombre!!!");
     }       
-
-    /* if(identifiant ==== localStorage.idt && laCouleurSelect === localStorage.couleur){
-
-        On déroule le code qui additionne dans le panier, et on va changer le nombre
-         affiché dans cart 
-
-    }else{ */
 
 });
 
